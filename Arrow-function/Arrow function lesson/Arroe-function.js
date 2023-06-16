7a413cbe67d43072f755c6b2b897136dc3979b24
Ashraf Mohamed <ashrafmohamed1176@gmail.com>
2023-06-16 15:08:27 +0300

    Edit file Arrow function && add file Arrow function Lesson

---
+++ Arrow-function/Arrow function lesson/Arroe-function.js
@@ -0,0 +1,22 @@
+// let print = function () {
+//     return 250;
+// }
+// console.log(print());
+
+// if you Have one block of code
+let print = _ => 550;
+console.log(print());
+
+// let type = function (num) {
+//     return num;
+// }
+
+// if you Have one parameter1
+let type = num => num;
+document.write(type(500))
+
+
+// if you Have a lot-of parameter2
+let type2 = (None, Ntow) => None + Ntow;
+
+    document.write(type2(10, 40))
