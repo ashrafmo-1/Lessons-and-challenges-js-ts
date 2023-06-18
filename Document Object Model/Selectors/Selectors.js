e5c5ddfd7421655d16b2219da98377415ed1cdef
Ashraf Mohamed <ashrafmohamed1176@gmail.com>
2023-06-18 20:39:03 +0300

    add Document Object Model File

---
+++ Document Object Model/Selectors/Selectors.js
@@ -0,0 +1,26 @@
+/* Document Object Model */
+// ##################################### //
+
+let myTagElement = document.getElementsByTagName("p");
+let myIdElement = document.getElementById("my-div");
+let myclassElement = document.getElementsByClassName("my-span");
+let myqueryElement = document.querySelector(".wellcon")
+let myElement = document.querySelectorAll("a")
+// ##################################### //
+
+console.log(myTagElement);
+console.log(myTagElement[0]);
+console.log(myIdElement);
+console.log(myclassElement);
+console.log(myqueryElement);
+console.log(myElement);
+// ##################################### //
+
+console.log(document.title)
+console.log(document.body)
+console.log(document.forms)
+console.log(document.forms[0].one.value)
+// ##################################### //
+
+console.log(document.links)
+console.log(document.links[1])
