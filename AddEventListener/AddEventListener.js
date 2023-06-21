99b0eb2ddcca0cfaeb6435752bef91072b663dce
Ashraf Mohamed <ashrafmohamed1176@gmail.com>
2023-06-21 23:19:21 +0300

    ADD AddEventListener

---
+++ AddEventListener/AddEventListener.js
@@ -0,0 +1,56 @@
+// Old experiment
+
+const pargraph = document.querySelector("p")
+
+// event on click Element
+// pargraph.onclick = one;
+// pargraph.onclick = tow;
+
+// function one() {
+//     console.log("hi Scripting Ashraf for onClick One")
+// };
+
+// function tow() {
+//     console.log("hi Scripting Ashraf for onClick Tow")
+// };
+
+
+
+// // AddEventListener { New experiment
+
+// // Examble One
+// pargraph.addEventListener("click", function () {
+//     console.log("hi Scripting Ashraf for addEventListener")
+// })
+
+// pargraph.addEventListener("click", one);
+// pargraph.addEventListener("click", tow);
+
+
+pargraph.onclick = function () {
+    let NEwP = pargraph.cloneNode(true)
+    NEwP.className = `newcopy`
+    document.body.appendChild(NEwP)
+}
+
+// prent text 
+// let colone = document.querySelector(".newcopy") // An Error
+// .newcopy.onclick = function () {
+
+// }
+
+document.addEventListener("click", function (e) {
+    if (e.target.className === 'newcopy') {
+        console.log("ash")
+    }
+});
+
+const hoho = document.getElementsByClassName("hello")
+
+document.addEventListener("click", function (ele) {
+    if (ele.target.className === 'hello') {
+
+        console.log("ashraf Welcom")
+
+    }
+})
