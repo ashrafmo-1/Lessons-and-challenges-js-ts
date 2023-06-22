cdd3c60b75c445cb2bff610e399a021547f142bf
Ashraf Mohamed <ashrafmohamed1176@gmail.com>
2023-06-22 17:42:04 +0300

    edit all classes names for more Error

--- allChalenge/order of numbers and elements/main.js
+++ allChalenge/order of numbers and elements/main.js
@@ -1,4 +1,9 @@
-const elementOne = document.querySelector("one")
-const elementTow = document.querySelector("tow")
+const elementOne = document.querySelector(".one")
+const elementTow = document.querySelector(".two")
 
-elementOne.title = elementOne.className
+
+elementOne.title = elementOne.className;
+elementTow.title = elementTow.className;
+
+elementOne.textContent = elementOne.className
+elementTow.textContent = `${elementTow.className} ${elementTow.attributes.length}`
