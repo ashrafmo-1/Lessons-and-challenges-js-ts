79d298b5246920c65ce87fffd6ca95c57fdc0be8
Ashraf Mohamed <ashrafmohamed1176@gmail.com>
2023-06-22 12:15:00 +0300

    add chalenge image ( src & alt ) && Edit More Elements Error

---
+++ chalenge image ( src & alt )/main.js
@@ -0,0 +1,11 @@
+// extinshion to chalenge
+let All = document.querySelector("body")
+All.style.background = "red"
+
+// chalenge
+const srcAndalt = document.images;
+
+for (index = 0; index < srcAndalt.length; index++) {
+    srcAndalt[index].src = "https://elzero.org/wp-content/themes/elzero/imgs/logo.png"
+    srcAndalt[index].alt = "Elzero Logo"
+}
