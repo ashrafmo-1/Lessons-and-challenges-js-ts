132aca55164c844ed62d62fae99e414e3e6ead38
Ashraf Mohamed <ashrafmohamed1176@gmail.com>
2023-06-24 14:27:06 +0300

    add new file 'BOM'

---
+++ Bom/methods  Alert, Confirm, Prompt/main.js
@@ -0,0 +1,21 @@
+/*
+All Item alert
+    msg Ok ONly
+*/
+window.alert("hi");
+this.alert("welcom");
+alert("hillo");
+
+// msg Ok && cancel
+let confirmMsg = confirm("hiaaa");
+console.log(confirmMsg);
+
+if (confirmMsg === true) {
+    console.log("Done")
+} else {
+    console.log("Dont Done")
+};
+
+// typy messame
+let promptMsg = prompt("type ", " type here ");
+document.write(promptMsg);
