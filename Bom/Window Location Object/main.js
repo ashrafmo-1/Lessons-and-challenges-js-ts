27ad8c83571723141fb5dd88791c8a9a133be63c
Ashraf Mohamed <ashrafmohamed1176@gmail.com>
2023-06-25 14:16:15 +0300

    Window Location Object AndWindow Open And Close

---
+++ Bom/Window Location Object/main.js
@@ -0,0 +1,16 @@
+console.log(location)
+console.log(location.href)
+
+location.href = "https://Google.com"
+location.href = "https://developer.mozilla.org/en-US/docs/Web/API/setInterval#example_2_alternating_two_colors"
+
+// location Host
+// location HostNmae
+
+console.log(location.host)
+console.log(location.hostname)
+
+console.log(location.protocol)
+
+location.replace("https://Googole.com") // remove All past Pages
+location.assign("https://Googole.com")
---
+++ Bom/Window Open And Close/main.js
@@ -0,0 +1,4 @@
+
+setTimeout(function () {
+    window.open("https://Google.com", "", "width=500,height=500,top=500,right=0")
+}, 2000)
