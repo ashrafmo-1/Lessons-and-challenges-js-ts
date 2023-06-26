d181b4bab854e49ae1c9daf7859ddc13aa90a67c
Ashraf Mohamed <ashrafmohamed1176@gmail.com>
2023-06-26 20:57:52 +0300

    add get element and onclick

---
+++ allChalenge/get element and onclick/main.js
@@ -0,0 +1,51 @@
+const div = document.querySelector("div")
+const span = document.querySelector("span")
+const p = document.querySelector("p")
+const article = document.querySelector("article")
+const section = document.querySelector("section")
+
+
+
+
+
+
+div.onclick = function () {
+    console.log("my div")
+}
+
+span.onclick = function () {
+    console.log("my span")
+}
+
+p.onclick = function () {
+    console.log("my paragraph")
+}
+
+article.onclick = function () {
+    console.log("my article")
+}
+
+section.onclick = function () {
+    console.log("my section")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* to chick for element */
+// console.log(div)
+// console.log(span)
+// console.log(p)
+// console.log(article)
+// console.log(section)
