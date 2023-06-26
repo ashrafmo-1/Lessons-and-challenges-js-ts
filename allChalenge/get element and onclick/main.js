bc41697d38ad51ae4f110ab9e0bfa023e4ad459c
Ashraf Mohamed <ashrafmohamed1176@gmail.com>
2023-06-26 23:28:06 +0300

    Edir some commints

--- allChalenge/get element and onclick/main.js
+++ allChalenge/get element and onclick/main.js
@@ -1,36 +1,34 @@
-// const div = document.querySelector("div")
-// const span = document.querySelector("span")
-// const p = document.querySelector("p")
-// const article = document.querySelector("article")
-// const section = document.querySelector("section")
+/*
 
-// div.onclick = function () {
-//     console.log("my div")
-// }
-// span.onclick = function () {
-//     console.log("my span")
-// }
-// p.onclick = function () {
-//     console.log("my paragraph")
-// }
-// article.onclick = function () {
-//     console.log("my article")
-// }
-// section.onclick = function () {
-//     console.log("my section")
-// }
+const div = document.querySelector("div")
+const span = document.querySelector("span")
+const p = document.querySelector("p")
+const article = document.querySelector("article")
+const section = document.querySelector("section")
 
+div.onclick = function () {
+    console.log("my div")
+}
+span.onclick = function () {
+    console.log("my span")
+}
+p.onclick = function () {
+    console.log("my paragraph")
+}
+article.onclick = function () {
+    console.log("my article")
+}
+section.onclick = function () {
+    console.log("my section")
+}
+
+*/
 
 /* dinamoc Examble */
 // make Array 
 [...document.body.children].forEach(function (element) {
     element.onclick = _ => console.log(`this is ${element.localName}`)
 })
-
-
-
-
-
 
 
 
