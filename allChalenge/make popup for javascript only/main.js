fbd751169136265a26811aaa8402c898254bbf62
Ashraf Mohamed <ashrafmohamed1176@gmail.com>
2023-06-28 00:43:46 +0300

    edit style for popup

--- allChalenge/make popup for javascript only/main.js
+++ allChalenge/make popup for javascript only/main.js
@@ -1,17 +1,27 @@
-const divElement = document.createComment("div");
-const h1Element = document.createElement("h1");
-const pElement = document.createElement("p")
-const btn = document.createElement("button")
+let allEl = document.createElement("div");
+let h1 = document.createElement("h1");
+let p = document.createElement("p");
+let btn = document.createElement("button");
 
-function timepopup() {
-    // add class for all Element
+function mkPopup() {
+    // add text content
+    h1.textContent = "welcom"
+    p.textContent = "welcom Ashraf Mohamed"
+    btn.textContent = "x"
 
-    // Add Text Content
-    h1Element.textContent = "Welcom"
-    divElement.textContent = "Welcom"
+    // add element in page
+    allEl.append(h1, p, btn);
+    document.body.append(allEl)
 
-    h1Element.className = "head"
-    divElement.className = "All div"
-    document.body(divElement)
+    // add style popup
+    document.body.style.cssText = "font-family: 'Roboto', sans-serif;"
+    allEl.style.cssText = "text-align: center; font-size: 22px; background-color: red; width: fit-content; margin: auto; padding: 10px 80px; border-radius: 15px; position: relative;"
+    btn.style.cssText = "position: absolute; top: -5px; right: -5px; border-radius: 50%; border: none; font-size: 19px; cursor: pointer;"
 
+    btn.addEventListener("click", function () {
+        btn.parentElement.remove()
+    })
 }
+
+// to run element in page
+setTimeout(mkPopup, 2000)
