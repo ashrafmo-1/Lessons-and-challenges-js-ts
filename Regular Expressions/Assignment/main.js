94c28f56461ec5e67697fc1602742b1725c21cbc
Ashraf Mohamed <ashrafmohamed1176@gmail.com>
2023-07-08 20:12:50 +0300

    Add the end assignment fot regular Expressions

--- Regular Expressions/Assignment/main.js
+++ Regular Expressions/Assignment/main.js
@@ -17,7 +17,30 @@ console.log(phone.match(phoneRe));
 console.log(phone.match(phoneRe2));
 console.log(`=`.repeat(30));
 
-// let link = "https://elzero.com"
-// let re = /https?:\/\/(?:[-\w]+\.)?([-\w]+)\.\w+(?:\.\w+)?\/?.*/ig;
-// console.log(link.match(re));
-// console.log(`=`.repeat(30));
+let url1 = 'elzero.org';
+let url2 = 'http://elzero.org';
+let url3 = 'https://elzero.org';
+let url4 = 'https://www.elzero.org';
+let url5 = 'https://www.elzero.org:8080/articles.php?id=100&cat=topics';
+let url6 = 'https://www.elzero.org:8080/articles.php?id=100&cat=topics/qewrre';
+
+let re = /(https?:\/\/)?(\w+)?\w+\w+(\w+)?\/?.*/ig;
+
+console.log(url1.match(re));
+console.log(url2.match(re));
+console.log(url3.match(re));
+console.log(url4.match(re));
+console.log(url5.match(re));
+console.log(url6.match(re));
+
+let date1 = "25/10/1982";
+let date2 = "25 - 10 - 1982";
+let date3 = "25 10 1982";
+let date4 = "25 10 82";
+
+let dateRe = /\d{2}\s?-?\s?\/?\d{2}\s?-?\s?\/?\d{2,4}/g; // Write Pattern Here
+
+console.log(date1.match(dateRe)); // "25/10/1982"
+console.log(date2.match(dateRe)); // "25 - 10 - 1982"
+console.log(date3.match(dateRe)); // "25 10 1982"
+console.log(date4.match(dateRe)); // "25 10 82"
