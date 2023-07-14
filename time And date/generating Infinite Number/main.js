85cba967e5fde75530c48fe4d1daeb97e9e97675
Ashraf Mohamed <ashrafmohamed1176@gmail.com>
2023-07-14 15:36:19 +0300

    add generating Infinite Number

---
+++ time And date/generating Infinite Number/main.js
@@ -0,0 +1,35 @@
+function* gNumber() {
+    let index = 0
+
+    while (true) {
+        yield index++
+    }
+}
+
+let generator = gNumber();
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.return("End")); // to End generator
