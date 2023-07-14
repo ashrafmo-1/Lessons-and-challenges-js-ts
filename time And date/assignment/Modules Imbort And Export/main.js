9696d9d97d0496e59a95b44b680ad860a809125d
Ashraf Mohamed <ashrafmohamed1176@gmail.com>
2023-07-15 02:47:27 +0300

    modified assignment add Modules Imbort And Export

---
+++ time And date/assignment/Modules Imbort And Export/main.js
@@ -0,0 +1,4 @@
+import calc from "./modone.js"
+import * as modOne from "./numTwo.js"
+
+console.log(calc(modOne.numOne, modOne.numTwo, modOne.numThree)); // 60
---
+++ time And date/assignment/Modules Imbort And Export/numTwo.js
@@ -0,0 +1,5 @@
+let a = 10; // Do Not Edit Names
+let b = 20; // Do Not Edit Names
+let c = 30; // Do Not Edit Names
+
+export { a as numOne, b as numTwo, c as numThree }
