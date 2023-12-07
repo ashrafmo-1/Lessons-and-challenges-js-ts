52280bc6c09d1c6595175646c3f438f975611822
ashraf mohamed <ashrafmohamed1176@gmail.com>
2023-12-07 07:56:55 +0200

    remove element from array

---
+++ RmoveArray.js
@@ -0,0 +1,11 @@
+// Reamove last element from the array
+let fruits = ['apple', 'banana', 'orange'];
+console.log(fruits); // ['apple', 'banana', 'orange']
+console.log(fruits.pop()); // lastFruit is 'orange'
+console.log(fruits); // ['apple', 'banana']
+
+// Reamove First element from the array
+let reada = ['taira', 'cora', 'basket'];
+console.log(reada);
+console.log(reada.shift());
+console.log(reada);
