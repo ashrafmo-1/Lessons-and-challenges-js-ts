2ba25e82d4913857c07459b4fd50e3396f47ba58
ashraf mohamed <ashrafmohamed1176@gmail.com>
2023-12-07 08:36:57 +0200

    control on arrays

---
+++ addArray.js
@@ -0,0 +1,15 @@
+// Add a new element to the array
+let names = ['ashraf', 'mohamed', 'ali', 'mahmoud'];
+console.log(names);
+names.push('d3bs');
+console.log(names);
+names.push('hoba');
+console.log(names);
+names.unshift('malek')
+console.log(names);
+
+let myArray = [1, 2, 3, 4];
+myArray.push(5); 
+console.log(myArray);
+myArray.unshift(0)
+console.log(myArray);
