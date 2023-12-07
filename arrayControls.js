bae8416e3e312fda586d6fbac98f00431d7bdf78
ashraf mohamed <ashrafmohamed1176@gmail.com>
2023-12-07 09:39:41 +0200

    finsh

--- arrayControls.js
+++ arrayControls.js
@@ -3,7 +3,6 @@ let arr2 = ['ash', 'raf', 'ram', 'i']
 console.log(arr.indexOf(10));
 console.log(arr2.indexOf('ash'));
 
-
 // splice built-in function
 let fruits = ['apple', 'banana', 'orange'];
 console.log(fruits);
@@ -18,13 +17,11 @@ console.log(subset);
 let numbers = [1, 2, 3, 4, 5, 6, 7];
 console.log(numbers.slice(0, 3));
 
-
 let fruits1 = ['apple', 'kiwi'];
 let fruits2 = ['grape', 'orange'];
 let fruits3 = ['banana'];
 let combinedFruits = fruits1.concat(fruits2); // combinedFruits is ['apple', 'kiwi', 'grape', 'orange']
 console.log(combinedFruits.concat(fruits3));
-
 
 let apis = ['products', 'closes', 'images', 'salary'];
 apis.forEach((fruit) => {
