7c25e984ab584b7801ff2534a85afce5b2bdef81
ashraf mohamed <ashrafmohamed1176@gmail.com>
2024-01-01 23:26:32 +0200

    for loops medels

--- forLoops.js
+++ forLoops.js
@@ -18,14 +18,34 @@ let myFriends = [1, 2, "AShraf", "Sheno", 3, 4, "Alaa", 6, "Ali"];
 // console.log(myFriends[3]);
 // console.log(myFriends[4]);
 
-// for (let i = 0; i < myFriends.length; i++) {
-//     console.log(myFriends[i]);
-// }
-
-let onlyNames = [];
 for (let i = 0; i < myFriends.length; i++) {
-    if (typeof myFriends[i] === "string") {
-        onlyNames.push(myFriends[i]);
-    }
+    console.log(myFriends[i]);
 }
-console.log(onlyNames);
+
+// let onlyNames = [];
+// for (let i = 0; i < myFriends.length; i++) {
+//     if (typeof myFriends[i] === "string") {
+//         onlyNames.push(myFriends[i]);
+//     }
+// }
+// // console.log(onlyNames);
+
+
+
+// let colors = ["Red", "Green", "Black"];
+
+// let models = [2020, 2021];
+
+// for (let i = 0; i < products.length; i++) {
+//     console.log("#".repeat(15));
+//     console.log(`# ${products[i]}`);
+//     console.log("#".repeat(15));
+//     console.log("Colors: ");
+//     for (let j = 0; j < colors.length; j++) {
+//     console.log(`- ${colors[j]}`);
+//     }
+//     console.log("Models: ");
+//     for (let k = 0; k < models.length; k++) {
+//     console.log(`- ${models[k]}`);
+//     }
+// }
