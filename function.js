5960848d1ee24ec72724959ba8cdc0af496f0c1f
ashraf mohamed <ashrafmohamed1176@gmail.com>
2024-01-03 14:37:55 +0200

    examples functions

--- function.js
+++ function.js
@@ -13,3 +13,32 @@ function sayWelcome(userName) {
 }
 
 sayWelcome('ashraf')
+sayWelcome('shenoo')
+
+
+
+// ==========================================================
+
+function sayHello(userName, age) {
+    if (age < 18) {
+        console.log(`App is Not Avalibale For You`);
+    } else {
+        console.log(`Hello ${userName} Your Age is ${age}`);
+    }
+}
+
+sayHello('Ashraf', 21)
+sayHello('Ali', 17)
+
+// ==========================================================
+
+function generateYears(start, end, notPrint) {
+    for (let i = start; i <= end; i++) {
+    if (i === notPrint) {
+        continue;
+    }
+    console.log(i);
+    }
+}
+
+generateYears(1982, 2000, 1999);
