ccab2561c0542a85f89394410081e3ab8e97fc01
ashraf mohamed <ashrafmohamed1176@gmail.com>
2024-01-03 12:56:14 +0200

    start function

---
+++ function.js
@@ -0,0 +1,15 @@
+/*
+    Function
+    # What Is Function ?
+    # User-Defined vs Built In
+    # Syntax + Basic Usage
+    # Example From Real Life
+    # Parameter + Argument
+    # Practical Example
+*/
+
+function sayWelcome(userName) {
+    console.log(`Welcome ${userName}`);
+}
+
+sayWelcome('ashraf')
