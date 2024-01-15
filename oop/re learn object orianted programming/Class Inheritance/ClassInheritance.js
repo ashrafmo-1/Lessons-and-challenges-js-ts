386d839d8e4c35b347bee24d6f39c10d601b8c21
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-01-15 15:45:07 +0200

    add class inheritans

---
+++ oop/re learn object orianted programming/Class Inheritance/ClassInheritance.js
@@ -0,0 +1,29 @@
+class Persons {
+    constructor(ID, userName, age){
+        this.i = ID;
+        this.n = userName;
+        this.ag = age;
+    }
+}
+
+// start make inhertance
+class Admin extends Persons{
+    constructor(ID, userName, age, access) {
+        super(ID, userName, age)
+        this.ac = access;
+    }
+}
+
+class Administrator extends Admin{
+    constructor(ID, userName, age, access, ability) {
+        super(ID, userName, age, access, ability);
+        this.ab = ability
+    }
+}
+
+// Persons
+let userOne = new Persons(0, 'ASH', 21);
+// Administrator
+let adminOne = new Admin(1, 'shenoo', 22, true)
+// Administrators
+let ad = new Administrator(1001, 'khaled', 30, false, 'max')
