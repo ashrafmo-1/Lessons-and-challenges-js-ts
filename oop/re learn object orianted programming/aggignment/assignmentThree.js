f388eb80071fcd83700c7cfe51c267f186004565
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-01-19 22:05:51 +0200

    assgignment three

---
+++ oop/re learn object orianted programming/aggignment/assignmentThree.js
@@ -0,0 +1,28 @@
+
+
+// Edit The Class
+class User {
+    #c
+    constructor(username, card) {
+        this.u = username;
+        this.#c = card;
+    }
+    showData() {
+        return `Hello ${this.u} Your Credit Card Number Is ${this.u}`
+    }
+}
+
+// Do Not Edit Anything Below
+
+let userOne = new User("Osama", "1234-5678-1234-5678");
+let userTwo = new User("Ahmed", "1234567812345678");
+let userThree = new User("Ghareeb", 1234567812345678);
+
+console.log(userOne.showData);
+// Hello Osama Your Credit Card Number Is 1234-5678-1234-5678
+console.log(userTwo.showData);
+// Hello Ahmed Your Credit Card Number Is 1234-5678-1234-5678
+console.log(userThree.showData);
+// Hello Ghareeb Your Credit Card Number Is 1234-5678-1234-5678
+console.log(userOne.c); // Prevent Accessing To Card Property Here
+// Undefined
