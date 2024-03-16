13646a47c3e4b92620e6c89a03162896ab702f3f
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-03-17 00:37:48 +0200

    new intro word file and solv some questions

--- assignmentsCours/assignnmentAI.js
+++ assignmentsCours/assignnmentAI.js
@@ -76,10 +76,58 @@ const longestString_exampleTwo = (arr) => {
 }
 
 // 9* Write a function that takes a number and returns true if it's prime, false otherwise.
+
+function InitialPreparations(num) {
+  if (num <= 1) {
+    return false
+  } else {
+    for(i = 2; i <= Math.sqrt(num); i++) {
+      if(num % i === 0){
+        return false
+      }
+    }
+  }
+  return true
+}
+console.log(InitialPreparations(2));
+console.log(InitialPreparations(3));
+console.log(InitialPreparations(5));
+console.log(InitialPreparations(7));
+console.log(InitialPreparations(11));
+console.log(InitialPreparations(13));
+
 // 10* Create a function that takes an array of numbers and returns a new array with only the unique numbers.
-// 11* Write a function that takes a string and returns the number of vowels in it.
+const unique = (arr) => {
+  return [...new Set(arr)];
+}
+console.log(unique([1,32,4,4,54,55,1,55]));
+console.log(typeof unique([1,32,4,4,54,55,1,55]));
+
+// 11* Writstateunction that takes a string and returns the number of vowels in it.
+const vowels = 'aeiouAEIOU';
+const count_vowels = (string) => {
+  let count = 0;
+  // for(i = 0; i < string.length; i++) {
+  for(let char of string) {
+    if(vowels.includes(char)) {
+      count++
+    }
+  }
+  return count;
+}
+console.log(count_vowels("hello world"));
+
 // 12* Create a function that takes an array of numbers and returns the average of all the numbers.
+const average = (array) => {
+  let sum = array.reduce((acc, current) => acc + current ,0);
+  return sum / array.length;
+}
+console.log(average([1,2,2,2,3,5,6,7,7,7,7,7]));
+
 // 13* Write a function that takes a number and returns its factorial.
+
+
+
 // 14* Create a function that takes an array of strings and returns a new array with the strings capitalized.
 // 15* Write a function that takes a string and checks if it's a palindrome.
 // 16* Create a function that takes an array of numbers and returns a new array with the numbers sorted in ascending order.
