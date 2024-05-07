7072f27f963ee22b8828af122bd34505ad375f04
Ashraf Qopiah <ashrafmohamed1176@gmail.com>
2024-05-07 21:15:18 +0300

    Array Reduce Transformation 30 day js

---
+++ problemSolving/30 days for javascrip/Array Reduce Transformation.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+  if (nums.length === 0) {
+    return init;
+  }
+
+  let result = init;
+
+  for (let i = 0; i < nums.length; i++) {
+    result = fn(result, nums[i]);
+  }
+
+  return result;
+};
